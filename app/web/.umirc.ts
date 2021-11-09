531182c0f6f4a36e72482300f53aafd94d798cb2
Ashoka <savebtc11@gmail.com>
2021-11-09 16:21:00 +0800

    feat(umi): open ssr

--- app/web/.umirc.ts
+++ app/web/.umirc.ts
@@ -4,8 +4,16 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
+  ssr: {
+    // dev 模式 服务端渲染交给 eggjs 处理
+    devServerRender: false,
+  },
+  hash: true,
+  outputPath: '../public',
+  manifest: {
+    fileName: '../../config/manifest.json',
+    publicPath: '',
+  }
 });
